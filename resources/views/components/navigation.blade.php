f503716193171e9095edf94d1208e46403e7e56f
juliush <juliush.dev@gmail.com>
2023-02-20 09:24:41 +0100

    reorder the navigation

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,68 +1,85 @@
-<nav class="flex flex-nowrap items-center justify-between p-6 bg-slate-100">
-    <div class="flex items-center flex-none">
-        <span class="font-semibold text-xl tracking-tight">E.A</span>
-    </div>
-    <x-splade-toggle class="relative flex-1 flex">
-        <x-splade-transition
-            class="absolute top-20 z-50 w-11/12 mx-auto bg-white shadow-lg rounded-md p-6 flex flex-col space-y-4"
-            show="toggled">
-            <x-nav-link href="{{ route('welcome') }}" :active="Route::is('welcome')">
-                {{ __('Home') }}
+    <nav
+        class="bg-gray-900 flex flex-nowrap space-x-5 justify-between items-center overflow-x-auto fixed left-0 top-0 z-50 w-full">
+        <div class="flex justify-end mr-4">
+            <x-nav-link href="{{ route('welcome') }}" :active="Route::is('welcome')" class="">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <span>Home</span>
             </x-nav-link>
-            @if (Auth::guest())
-                <x-nav-link href="{{ route('events.index') }}" :active="Route::is('events.index')">
-                    {{ __('Events') }}
+            @canany(['if_admin', 'if_company'])
+                <x-nav-link href="{{ route('dashboard') }}" :active="Route::is('dashboard')">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
+                    </svg>
+                    <span>Dashboard</span>
                 </x-nav-link>
-                <x-nav-link href="{{ route('login') }}" :active="Route::is('login')">
-                    {{ __('Login') }}
+            @endcanany
+            <x-nav-link href="{{ route('events.index') }}" :active="Route::is('events.index')">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                </svg>
+                <span>Events</span>
+            </x-nav-link>
+            @can('if_admin')
+                <x-nav-link :href="route('forms.index')" :active="Route::is('forms.index')">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+                    </svg>
+                    <span>Forms</span>
+                </x-nav-link>
+                <x-nav-link :href="route('groups.index')" :active="Route::is('groups.index')">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M21 11.25v8.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 109.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1114.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+                    </svg>
+                    <span>Packages</span>
+                </x-nav-link>
+            @endcan
+            @if (Auth::guest())
+                <x-nav-link href="{{ route('login') }}" :active="Route::is('login')" class="p-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                    </svg>
+                    <span>Login</span>
                 </x-nav-link>
                 <x-nav-link href="{{ route('register') }}" :active="Route::is('register')">
-                    {{ __('Register') }}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                    </svg>
+                    <span>Register</span>
                 </x-nav-link>
-            @else
-                <hr class="md:hidden">
-                <x-nav-link href="{{ route('dashboard') }}" :active="Route::is('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-nav-link>
-                <x-nav-link href="{{ route('events.index') }}" :active="Route::is('events.index')">
-                    {{ __('Events') }}
-                </x-nav-link>
-                @can('if_admin')
-                    <x-nav-link :href="route('forms.index')" :active="Route::is('forms.index')">
-                        {{ __('Forms') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('groups.index')" :active="Route::is('groups.index')">
-                        {{ __('Form Groups') }}
-                    </x-nav-link>
-                @endcan
-                <hr class="md:hidden">
-                <x-nav-link href="{{ route('profile.edit') }}" :active="Route::is('profile.edit')">
-                    {{ __('Profile') }}
-                </x-nav-link>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                    @csrf
-                    <button type='submit'
-                        class="block w-full p-3 text-sm text-right underline decoration-sky-500 underline-offset-8">
-                        {{ __('Logout') }}
-                    </button>
-                </form>
             @endif
-        </x-splade-transition>
-        <div class="block ml-auto">
-            <button
-                class="flex items-center px-3 py-2 border rounded border-slate-400 hover:text-slate-500 hover:border-slate-500"
-                @click.prevent="toggle">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="fill-current w-6 h-6" v-show="!toggled">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" />
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6" v-show="toggled">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25" />
-                </svg>
-            </button>
+            @canany(['if_admin', 'if_company'])
+                <x-nav-link href="{{ route('profile.edit') }}" :active="Route::is('profile.edit')">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+                    <span>Profile</span>
+                </x-nav-link>
+                <x-nav-link href="{{ route('logout') }}" method="POST" :active="false">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                    </svg>
+                    <span>Logout</span>
+                </x-nav-link>
+            @endcanany
         </div>
-    </x-splade-toggle>
-</nav>
+    </nav>
