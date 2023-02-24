c9b99316240b721bed810e1addf52adf216675dc
Julius Herbert <juliush.dev@gmail.com>
2023-02-24 13:09:59 +0100

    A company can only subscribe to one event form group

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -181,7 +181,7 @@
                                                             ->first()->id;
                                                     @endphp
                                                     <Link href="{{ route('subscriptions.store') }}" method="POST"
-                                                        data="{subscription_option: {{ $subscription_option }}}"
+                                                        v-bind:data="{subscription_option: '{{ $subscription_option }}'}"
                                                         class="text-white text-sm px-2 py-1 bg-blue-500 rounded  shadow-md flex space-x-2 items-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
