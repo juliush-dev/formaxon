8c8515cf2c707ec396f1808a0e99da627dca4f82
Julius Herbert <juliush.dev@gmail.com>
2023-02-24 12:06:40 +0100

    Restyle the app header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,11 @@
-<div class="min-h-screen bg-gray-100">
+<div class="min-h-screen overflow-hidden">
     <x-navigation />
     @isset($header)
-        <header class="bg-white shadow p-4 mb-5 text-gray-800 sticky top-12 z-10">
+        <header class="bg-gray-900 text-white shadow p-4 sticky top-16 z-10">
             {{ $header }}
         </header>
     @endisset
-    <main class="px-4">
+    <main class="p-4">
         {{ $slot }}
     </main>
 </div>
